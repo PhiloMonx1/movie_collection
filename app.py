1786a06e087dda4c96bbeeff1236e4fee941eef7
2m <philomonx1@gmail.com>
2022-07-14 03:04:31 +0900

    [fix] 로그아웃 수정, 로그인 페이지 패스워드 노출 버튼 추가

--- app.py
+++ app.py
@@ -54,10 +54,10 @@ def sign_in():
             'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
         # JWT 버전 문제 ------------------------------------------------------------------ #
-        # token = jwt.encode(payload, SECRET_KEY,
-        #                    algorithm='HS256').decode('utf-8')
         token = jwt.encode(payload, SECRET_KEY,
-                           algorithm='HS256')
+                           algorithm='HS256').decode('utf-8')
+        # token = jwt.encode(payload, SECRET_KEY,
+        #                    algorithm='HS256')
 
         return jsonify({'result': 'success', 'token': token})
     # 찾지 못하면
